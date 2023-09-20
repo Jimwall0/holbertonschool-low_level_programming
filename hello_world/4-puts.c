f5258c1d23cb5c30f72d37605282c6d126f4c627
Tsim Muaj <tyang4204@gmail.com>
2023-09-20 11:23:17 -0700

    added a quote

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*The main place to run other programs*/
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
   return(0);
 }
