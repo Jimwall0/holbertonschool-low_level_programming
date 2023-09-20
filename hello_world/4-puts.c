68295b862f47ae061ce989a4ca9bfb28b11f8e76
Tsim Muaj <tyang4204@gmail.com>
2023-09-20 11:35:05 -0700

    somthing

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include <stdio.h> /*Include the necessary header file for the puts function*/
-/*The main place to run other programs*/
+#include <stdio.h> /*Neccassary Files*/
+/*
+ *main - to print using puts function
+ *return
+ *The main place to run other programs
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+/*Main*/
