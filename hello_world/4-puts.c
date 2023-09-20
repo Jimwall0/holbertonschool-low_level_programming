4b9119f6eb98d24ab3ac86cd2d957e0dac11f733
Tsim Muaj <tyang4204@gmail.com>
2023-09-20 11:45:26 -0700

    Fixed comments to be Betty approved

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h> /*Neccassary Files*/
-/*
- *main - to print using puts function
- *return
- *The main place to run other programs
-*/
+/**
+ * main - to print using puts function
+ * Return: 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-/*Main*/
