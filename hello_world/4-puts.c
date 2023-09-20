b38783e8edf257b0cdb504d28ddde0501f93fbda
Tsim Muaj <tyang4204@gmail.com>
2023-09-20 11:20:47 -0700

    I thing it workds?

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
-\*The main place to run other programs*\
-int main()
+#include <stdio.h> /*Include the necessary header file for the puts function*/
+/*The main place to run other programs*/
+int main(void)
 {
-  put("Programming is like building a multilingual puzzle\n");
+  puts("Programming is like building a multilingual puzzle");
   return(0);
 }
