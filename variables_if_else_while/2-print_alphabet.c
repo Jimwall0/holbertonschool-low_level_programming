0a4e12754f3a05cfece100ce636c8c49c9585b78
Tsim Muaj <tyang4204@gmail.com>
2023-09-25 09:47:13 -0700

    insomnia

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - will and should run my programs
+ * Return: hopefully it will return 0
+ */
+int main (void)
+{
+char aChar;
+for (aChar = 'a'; aChar <='z'; aChar++)
+{
+putchar(aChar);
+}
+putchar('\n');
+return (0);
+}
