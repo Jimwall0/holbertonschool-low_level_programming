01e836ce01a0e907ce04dd8fd081628e7473b5a5
Tsim Muaj <tyang4204@gmail.com>
2023-09-26 13:16:05 -0700

    10x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints 10 times the alphabet
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+char car;
+int num;
+for (num = 0; num < 10; num++)
+{      
+for (car = 'a'; car <= 'z'; car++)
+{
+_putchar(car);
+}
+_putchar('\n');
+}
+}
