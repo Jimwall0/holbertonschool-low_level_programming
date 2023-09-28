345c16a6fdcdbcd563fa91e70c694f3eb6423133
Tsim Muaj <tyang4204@gmail.com>
2023-09-28 11:04:41 -0700

    changed negative print

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -6,7 +6,7 @@
  */
 int print_sign(int n)
 {
-if (n < 0)
+if (n > 0)
 {
 _putchar('+');
 return (1);
@@ -18,7 +18,7 @@ return (0);
 }
 else if (n < 0)
 {
-_putchar('0');
+_putchar('-');
 return (-1);
 }
 }
