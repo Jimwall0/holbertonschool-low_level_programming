c3254a3ea072eb5ff03db8273d619e8fb20d4379
Tsim Muaj <tyang4204@gmail.com>
2023-09-28 11:29:32 -0700

    tried last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - should print the last digit
+ * @x: last digit to be printed
+ * Return: last digit
+ */
+int print_last_digit(int x)
+{
+int tot = x % 10;
+return (tot);
+}
