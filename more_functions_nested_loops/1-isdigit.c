0ace77912a9c16c5279251a53b95ebb311ef5fc0
Tsim Muaj <tyang4204@gmail.com>
2023-09-29 10:21:15 -0700

    added isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - checks int for digit
+ * @c: integer thats digit checked
+ * return: 0 or 1
+ */
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
