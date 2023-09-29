abc96254617677bdf048a884ca627f7f2188f946
Tsim Muaj <tyang4204@gmail.com>
2023-09-29 10:14:08 -0700

    fixed some errors

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,12 @@
 #include "main.h"
+/**
+ * _isupper - checks for uppercase
+ * @c: character to be checked
+ * Return: 1 or 0 depending on the captialization
+ */
 int _isupper(int c)
 {
-if (c >= 'A' && c <- 'Z')
+if (c >= 'A' && c <= 'Z')
 {
 _putchar(c);
 return (1);
