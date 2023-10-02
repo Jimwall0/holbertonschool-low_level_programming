785cda9ab6c33cdde8a1da21f06ac358c851f4d8
Tsim Muaj <tyang4204@gmail.com>
2023-10-01 18:29:33 -0700

    moved if outside of loop

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,20 +7,17 @@ void print_diagonal(int n)
 {
 int line;
 int num;
-for (num = 0; num < n; num++)
-{
 if (n <= 0)
 {
 _putchar('\n');
 }
-else
+for (num = 0; num < n; num++)
 {
-for (line = 1; line < num; line++)
+for (line = 0; line < num; line++)
 {
 _putchar(' ');
 }
 _putchar('\\');
 _putchar('\n');
-}
 }
 }
