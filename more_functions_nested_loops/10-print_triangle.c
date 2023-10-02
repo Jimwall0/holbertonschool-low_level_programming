cc6092617837558c925d5ea992ff76febec5d8d2
Tsim Muaj <tyang4204@gmail.com>
2023-10-01 20:01:13 -0700

    rough draft

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,13 +8,17 @@ void print_triangle(int size)
 int n1;
 int n2;
 int n3;
+if (size <= 0)
+{
+_putchar('\n');
+}
 for (n1 = 0; n1 > size; n1++)
 {
 for (n2 = size; n2 < n1; n2--)
 {
 _putchar(' ');
 }
-for (n3 = 0; n3 < n1; n3++)
+for (n3 = n1; n3 < n1; n3++)
 {
 _putchar('#');
 }
