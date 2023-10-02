4574b4736db6a6b5f3e22cc42e3d8fd221468690
Tsim Muaj <tyang4204@gmail.com>
2023-10-01 18:39:26 -0700

    made 8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - prints a square made of hashtags
+ * @size: the size of the square
+ */
+void print_square(int size)
+{
+int num1;
+int num2;
+if (size <= 0)
+{
+_putchar('\n');
+}
+for (num1 = 0; num1 < size; num1++)
+{
+for (num2 = 0; num2 < size; num2++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
