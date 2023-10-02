d269725901bcb470ea6b8df362488169012bcffd
Tsim Muaj <tyang4204@gmail.com>
2023-10-02 11:37:25 -0700

    added 7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - prints the second half of array
+ * @str: the array in question
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int i = 0;
+int j = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+while (str[j] != '\0')
+{
+if (j >= i / 2)
+{
+_putchar(str[j]);
+}
+j++;
+}
+}
