d68b7689ad27055d630faeb09ff3718864319e53
Tsim Muaj <tyang4204@gmail.com>
2023-10-01 19:12:13 -0700

    priority

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,9 +8,9 @@ int main(void)
 int num;
 for (num = 1; num <= 100; num++)
 {
-if (num % 3 == 0)
+if (num % 3 == 0 && num % 5 == 0)
 {
-printf("Fizz");
+printf("FizzBuzz");
 putchar(' ');
 }
 else if (num % 5 == 0)
@@ -18,9 +18,9 @@ else if (num % 5 == 0)
 printf("Buzz");
 putchar(' ');
 }
-else if (num % 3 == 0 && num % 5 == 0)
+else if (num % 3 == 0)
 {
-printf("FizzBuzz");
+printf("Fizz");
 putchar(' ');
 }
 else
