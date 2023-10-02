4a7e89bc557f4e9413e00dc400d72a1f578d631c
Tsim Muaj <tyang4204@gmail.com>
2023-10-02 10:50:22 -0700

    fixed some mistake

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,14 @@
  */
 void print_rev(char *s)
 {
-int i;
+int i = 0;
 int j;
 while (s[i] != '\0')
 {
-i--;
 }
-for (j = i; j > 0; j--)
+for (j = i - 1; j > 0; j--)
 {
 _putchar(s[j]);
 }
+_putchar('\n');
 }
