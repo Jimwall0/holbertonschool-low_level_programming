6855a8f309942165262050156c59f05b98609bd2
Tsim Muaj <tyang4204@gmail.com>
2023-10-02 11:22:21 -0700

    added 6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - puts every other character down
+ * @str: array of char to be printed
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+if (str[i] % 2 == 0)
+{
+_putchar(str[i]);
+}
+i++;
+}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ void _puts(char *str);
 void _putchar(char);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
 
 #endif
