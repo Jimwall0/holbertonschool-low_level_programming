549a4a0fc534af5c20962113c618c6a52bdf634d
Tsim Muaj <tyang4204@gmail.com>
2023-10-02 20:37:14 -0700

    changed to printf

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_array - prints a array
  * @a: the array
@@ -10,6 +11,7 @@ void print_array(int *a, int n)
 int i;
 for (i = 0; i < n; i++)
 {
-_putchar(a[i]);
+printf("%d", a[i]);
 }
+putchar('\n');
 }
