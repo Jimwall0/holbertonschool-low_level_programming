bf67763a7aa6b8bbe3796a0705c2ad3c3183d996
Tsim Muaj <tyang4204@gmail.com>
2023-10-02 20:31:15 -0700

    added 8-print_array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_array - prints a array
+ * @a: the array
+ * @n: the amount to be printed
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar(a[i]);
+}
+}
