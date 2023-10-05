44893890a1c62f93e0929a74b71e5c4717483254
Tsim Muaj <tyang4204@gmail.com>
2023-10-05 09:00:30 -0700

    changed condition to if

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -4,13 +4,17 @@
  * @s: return pointer
  * @b: char that to be set
  * @n: an unsigned int reping the byte
+ * Return: pointer s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int num;
-for (num = 0; s[num] != '\0' && num < n; num++)
+for (num = 0; s[num] != '\0'; num++)
+{
+if (num < n)
 {
 s[num] = b;
+}
 }
 return (s);
 }
