00370d37ba1dfc8fb17d10e4683dbc9bf6fe0613
Tsim Muaj <tyang4204@gmail.com>
2023-10-05 07:26:49 -0700

    basd cap on delemiters

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,11 +7,24 @@
 char *cap_string(char *arr)
 {
 int num;
+char s[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+int loop;
 for (num = 0; arr[num] != '\0'; num++)
+{
+if (num == 0)
 {
 if (arr[num] >= 'a' && arr[num] <= 'z')
 {
 arr[num] = arr[num] + 32;
+}
+}
+for (loop = 0; s[loop] != '\0'; loop++)
+{
+if (arr[num] == s[loop])
+{
+num++;
+arr[num] = arr[num] + 32;
+}
 }
 }
 return (arr);
