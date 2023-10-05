bfeefb7bec6fc3b77c4abffc0a99d08ce47aa994
Tsim Muaj <tyang4204@gmail.com>
2023-10-05 08:53:33 -0700

    changed sign

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -8,7 +8,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int num;
-for (num = 0; s[num] != '\0' && num <= n; num++)
+for (num = 0; s[num] != '\0' && num < n; num++)
 {
 s[num] = b;
 }
