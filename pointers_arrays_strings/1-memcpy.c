2961c92e495ed8453c7c7e7581f61b16ea0814ae
Tsim Muaj <tyang4204@gmail.com>
2023-10-05 09:29:11 -0700

    added memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int num;
+for (num = 0; src[num] != '\0' && num < n; num++)
+{
+dest[num] = src[num];
+}
+return (dest);
+}
