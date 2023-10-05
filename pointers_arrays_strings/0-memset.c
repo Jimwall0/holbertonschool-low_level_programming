831c9154f5ac338086cacad7da86abcc36ea4310
Tsim Muaj <tyang4204@gmail.com>
2023-10-05 08:52:02 -0700

    betty

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,8 +1,14 @@
 #include "main.h"
+/**
+ * _memset - sets a memory
+ * @s: return pointer
+ * @b: char that to be set
+ * @n: an unsigned int reping the byte
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int num;
-for (num = 0; s[num] != '\0' && num < n; num++)
+for (num = 0; s[num] != '\0' && num <= n; num++)
 {
 s[num] = b;
 }
