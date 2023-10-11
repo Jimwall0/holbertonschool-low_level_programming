4eaf49481efd7b135a165b0ae8be20b22f5559a4
Tsim <tyang4204@gmail.com>
2023-10-11 10:55:16 -0700

    think I got it

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * strlen_recursion - returns the length of a string
+ * @s: string we need to find the length of.
+ * Return: How long is the string?
+ */
+int _strlen_recursion(char *s)
+{
+  int num;
+  num = *s;
+  if (*s)
+    {
+      _strlen_recursion(s + 1);
+    }
+  return (num);
+}
--- recursion/test.c
+++ recursion/test.c
@@ -1,6 +1,16 @@
 #include "main.h"
-int main (void)
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-  _puts_recursion("Puts with recursion");
-  return (0);
+    int n;
+
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+    return (0);
 }
