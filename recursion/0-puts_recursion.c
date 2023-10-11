343938a542f068d357d615b74857df2ecd70f50b
Tsim <tyang4204@gmail.com>
2023-10-11 07:56:29 -0700

    puts_recursion.c added

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "_putchar.c"
+#include "main.h"
+/**
+ * _puts_recursion - print a string followed by a new line
+ * @s: char array that should be a string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+  if (*s == '\0')
+    {
+      _putchar('\n');
+      return;
+    }
+  else if (*s)
+    {
+      _putchar(*s);
+      _puts_recursion(s + 1);
+    }
+}
