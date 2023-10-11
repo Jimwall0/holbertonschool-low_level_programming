64d257b676b495b2ce896e2bc9944b3ecadf5be7
Tsim <tyang4204@gmail.com>
2023-10-11 12:41:52 -0700

    save stop

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - Write a function that returns the factorial of a given number
+ * @n: the number
+ * Return: the factoorial
+ */
+int factorial(int n)
+{
+  int num = 1;
+  if ()
+    {
+      num++;
+      n = n * factorial(n - num);
+    }
+  return (n);
+}
--- recursion/main.h
+++ recursion/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+int factorial(int n);
 int _strlen_recursion(char *s);
 void _puts_recursion(char *s);
 int _putchar(char c);
