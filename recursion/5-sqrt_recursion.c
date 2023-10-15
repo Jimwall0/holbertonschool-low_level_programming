2aebb61107b8d961d74b31d17a38dae9f571d8f4
Tsim <tyang4204@gmail.com>
2023-10-15 13:31:54 -0700

    tried true method

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - gets the natural square root
+ * @n: number being squared
+ * Return: natural square
+ */
+int _sqrt_recursion(int n)
+{
+  if (n < 0)
+    {
+      return (-1);
+    }
+  return countroot(n, 0);
+}
+
+int countroot(int num, int root)
+{
+  if (num < 0)
+    {
+      return (-1);
+    }
+  else if (root * root == num)
+    {
+      return (root);
+    }
+  else
+    {
+      return countroot(num, root + 1);
+    }
+}
