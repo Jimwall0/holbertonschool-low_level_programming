3611190340dd313beb9dfd0e29710d41fceed655
Tsim <tyang4204@gmail.com>
2023-10-15 13:38:52 -0700

    betty

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -6,25 +6,24 @@
  */
 int _sqrt_recursion(int n)
 {
-  if (n < 0)
-    {
-      return (-1);
-    }
-  return countroot(n, 0);
+if (n < 0)
+{
+return (-1);
 }
-
+return (countroot(n, 0));
+}
 int countroot(int num, int root)
 {
-  if (num < 0 || root > num)
-    {
-      return (-1);
-    }
-  else if (root * root == num)
-    {
-      return (root);
-    }
-  else
-    {
-      return countroot(num, root + 1);
-    }
+if (num < 0 || root > num)
+{
+return (-1);
+}
+else if (root * root == num)
+{
+return (root);
+}
+else
+{
+return (countroot(num, root + 1));
+}
 }
