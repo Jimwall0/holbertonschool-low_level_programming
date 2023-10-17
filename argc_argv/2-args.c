1463872cfc2b9d07b330e17e027a399fd7184209
Tsim <tyang4204@gmail.com>
2023-10-16 21:18:09 -0700

    betty

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+/**
+ * main - runs
+ * @argc: length of the array
+ * @argv: actual product inside the array
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
-  int num = 0;
-  while (num < argc)
-    {
-      printf("%s\n", argv[num]);
-      num++;
-    }
-  return (0);
+int num = 0;
+while (num < argc)
+{
+printf("%s\n", argv[num]);
+num++;
+}
+return (0);
 }
