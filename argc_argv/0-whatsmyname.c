eaa121bce6816363048bf1c7e832ce257d07c2cc
Tsim <tyang4204@gmail.com>
2023-10-16 20:45:24 -0700

    figured out how to exclude

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,11 +2,12 @@
 /**
  * main - should run the program
  * Return: int
- * @argc - int holding the length of the string
- * @argv - array
+ * @argc: int holding the length of the string
+ * @argv: array
  */
 int main(int argc, char *argv[])
 {
+(void) argc;
 printf("%s\n", argv[0]);
-return (argc);
+return (0);
 }
