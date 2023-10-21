a8caa84954990255c8cdbcf61e7581c2da4edd5d
Tsim <tyang4204@gmail.com>
2023-10-21 14:38:38 -0700

    fixed code

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,12 +2,20 @@
 /**
  * create_array - creates an array within a certain size
  * @size: the limit
- * @c: name
+ * @c: value
  * Return: actual pointer
  */
 char *create_array(unsigned int size, char c)
 {
-  char *array = malloc(sizeof(int) * size);
-  array[0] = c;
+  char *array = malloc(sizeof(unsigned int) * size);
+  unsigned int num;
+  if (size == 0)
+    {
+      return (NULL);
+    }
+  for (num = 0; num < size; num++)
+    {
+      array[num] = c;
+    }
   return (array);
 }
