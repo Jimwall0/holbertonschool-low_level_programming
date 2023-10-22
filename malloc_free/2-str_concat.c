9f96b30d1051fc6431e78100c32e6692f9c1365b
Tsim <tyang4204@gmail.com>
2023-10-22 11:48:11 -0700

    betty

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,29 +8,29 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-  int loop;
-  char *array;
-  if (s1 == NULL)
-    {
-      s1 = "";
-    }
-  if (s2 == NULL)
-    {
-      s2 = "";
-    }
-  array = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
-  if (array == NULL)
-    {
-      return (NULL);
-    }
-  for (loop = 0; s1[loop] != '\0'; loop++)
-    {
-      array[loop] = s1[loop];
-    }
-  for (loop = 0; s2[loop] != '\0'; loop++)
-    {
-      array[loop + _strlen(s1)] = s2[loop];
-    }
-  array[loop + 1 + _strlen(s1)] = '\0';
-  return (array);
+int loop;
+char *array;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+array = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
+if (array == NULL)
+{
+return (NULL);
+}
+for (loop = 0; s1[loop] != '\0'; loop++)
+{
+array[loop] = s1[loop];
+}
+for (loop = 0; s2[loop] != '\0'; loop++)
+{
+array[loop + _strlen(s1)] = s2[loop];
+}
+array[loop + 1 + _strlen(s1)] = '\0';
+return (array);
 }
