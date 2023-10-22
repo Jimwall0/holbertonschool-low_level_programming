94f981a7f7610be578ea55173103a44628f8f73d
Tsim <tyang4204@gmail.com>
2023-10-22 04:42:47 -0700

    final

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -6,31 +6,35 @@
  */
 char *_strdup(char *str)
 {
-  int loop = 0;
-  char *dup;
-  if (str == NULL)
-    {
-      return (NULL);
-    }
-  dup = malloc(sizeof(char) * (_strlen(str) + 1));
-  if (dup == NULL)
-  {
-    return (NULL);
-  }
-  while (loop < _strlen(str))
-    {
-      dup[loop] = str[loop];
-    }
-  dup[loop] = '\0';
-  return (dup);
+int num = 0;
+char *dup;
+if (str == NULL)
+{
+return (NULL);
 }
-
+dup = malloc(sizeof(char) * (_strlen(str) + 1));
+if (dup == NULL)
+{
+return (NULL);
+}
+while (num < _strlen(str))
+{
+dup[num] = str[num];
+num++;
+}
+return (dup);
+}
+/**
+ * _strlen - gets the length of a string
+ * @s: the string
+ * Return: the size of the string
+ */
 int _strlen(char *s)
 {
-  int loop = 0;
-  while (s[loop] != '\0')
-    {
-      loop++;
-    }
-  return (loop);
+int loop = 0;
+while (s[loop] != '\0')
+{
+loop++;
+}
+return (loop);
 }
