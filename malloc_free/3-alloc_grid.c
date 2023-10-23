2f34e96123e266652dc624d6bfd574f74c2b5358
Tsim <tyang4204@gmail.com>
2023-10-23 07:55:05 -0700

    betty

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -7,40 +7,40 @@
  */
 int **alloc_grid(int width, int height)
 {
-  int **array, loop, hoop, x, y;
+int **array, loop, hoop, x, y;
 
-  if (width <= 0 || height <= 0)
-    {
-      return (NULL);
-    }
-  array = malloc (sizeof(int *) * height);
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+array = malloc(sizeof(int *) * height);
 
-  if (array == NULL)
-    {
-      free(array);
-      return (NULL);
-    }
+if (array == NULL)
+{
+free(array);
+return (NULL);
+}
 
-  for (loop = 0; loop < height; loop++)
-    {
-      array[loop] = malloc(sizeof(int) * width);
-      if (array[loop] == NULL)
-	{
-	  for (hoop = loop; hoop >= 0; hoop--)
-	    {
-	      free(array[hoop]);
-	    }
-	  free(array);
-	  return (NULL);
-	}
-    }
+for (loop = 0; loop < height; loop++)
+{
+array[loop] = malloc(sizeof(int) * width);
+if (array[loop] == NULL)
+{
+for (hoop = loop; hoop >= 0; hoop--)
+{
+free(array[hoop]);
+}
+free(array);
+return (NULL);
+}
+}
 
-  for (x = 0; x < height; x++)
-    {
-      for (y = 0; y < width; y++)
-	{
-	  array[x][y] = 0;
-	}
-    }
-  return (array);
+for (x = 0; x < height; x++)
+{
+for (y = 0; y < width; y++)
+{
+array[x][y] = 0;
+}
+}
+return (array);
 }
