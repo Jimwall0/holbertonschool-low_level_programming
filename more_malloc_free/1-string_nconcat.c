0bc8848b5ec0a7adacc4cf4464499f59d4e2465d
Tsim <tyang4204@gmail.com>
2023-10-23 16:29:31 -0700

    final change

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -8,44 +8,45 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  char *array;
-  unsigned int num = 0;
-  if (s1 == NULL)
-    {
-      s1 = "";
-    }
-  if (s2 == NULL)
-    {
-      s2 = "";
-    }
-  array = malloc(_strlen(s1) + _strlen(s2) - 1);
-  if (array== NULL)
-    {
-      return (array);
-    }
-  while (num < _strlen(s1))
-    {
-      array[num] = s1[num];
-      num++;
-    }
-  num = 0;
-  if (n >= _strlen(s2))
-    {
-      while (num + _strlen(s1) < _strlen(s2) + _strlen(s1))
-	{
-	  array[num + _strlen(s1)] = s2[num];
-	  num++;
-	}
-    }
-  else
-    {
-      while (num + _strlen(s1) < _strlen(s1) + n)
-	{
-	  array[num + _strlen(s1)] = s2[num];
-	  num++;
-	}
-    }
-  return (array);
+char *array;
+unsigned int num = 0;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+array = malloc(_strlen(s1) + _strlen(s2) - 1);
+if (array== NULL)
+{
+free(array);
+return (array);
+}
+while (num < _strlen(s1))
+{
+array[num] = s1[num];
+num++;
+}
+num = 0;
+if (n >= _strlen(s2))
+{
+while (num + _strlen(s1) < _strlen(s2) + _strlen(s1))
+{
+array[num + _strlen(s1)] = s2[num];
+num++;
+}
+}
+else
+{
+while (num + _strlen(s1) < _strlen(s1) + n)
+{
+array[num + _strlen(s1)] = s2[num];
+num++;
+}
+}
+return (array);
 }
 /**
  * _strlen - gets length of string
