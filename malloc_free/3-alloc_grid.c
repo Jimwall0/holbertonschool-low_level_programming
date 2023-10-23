1789461aca583df9dd9f6738ce2882f9b0620d87
Tsim <tyang4204@gmail.com>
2023-10-23 07:26:33 -0700

    added free at null if

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -7,31 +7,36 @@
  */
 int **alloc_grid(int width, int height)
 {
-int **array, num, loop, hoop;
-if (width <= 0 || height <= 0)
-{
-return (NULL);
-}
-array = malloc(sizeof(int *) * height);
-if (array == NULL)
-{
-return (NULL);
-}
-for (num = 0; num < width; num++)
-{
-array[num] = malloc(sizeof(int) * width);
-if (array[num] == NULL)
-{
-return (NULL);
-}
-}
-for (loop = 0; loop < height; loop++)
-{
-for (hoop = 0; hoop < width; hoop++)
-{
-array[loop][hoop] = 0;
-}
-}
-free(array);
-return (array);
+  int **array, num, num2, loop, hoop;
+  if (width <= 0 || height <= 0)
+    {
+      return (NULL);
+    }
+  array = malloc(sizeof(int *) * height);
+  if (array == NULL)
+    {
+      free(array);
+      return (NULL);
+    }
+  for (num = 0; num < width; num++)
+    {
+      array[num] = malloc(sizeof(int) * width);
+      if (array[num] == NULL)
+	{
+	  for (num2 = num; num2 >= num; num2--)
+	    {
+	      free(array[num]);
+	    }
+	  free(array);
+	  return (NULL);
+	}
+    }
+  for (loop = 0; loop < height; loop++)
+    {
+      for (hoop = 0; hoop < width; hoop++)
+	{
+	  array[loop][hoop] = 0;
+	}
+    }  
+  return (array);
 }
