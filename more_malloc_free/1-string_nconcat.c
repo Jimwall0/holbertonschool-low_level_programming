c56cd18454200769de12219b96cc59e15ad61ec5
Tsim <tyang4204@gmail.com>
2023-10-23 15:06:29 -0700

    added strlen

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -8,54 +8,63 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  char *array;
-  unsigned int num, s1len, s2len;
-  if (s1 == NULL)
-    {
-      s1 = "";
-    }
-  if (s2 == NULL)
-    {
-      s2 = "";
-    }
-  for (num = 0; s2[num] != '\0'; num++)
-    {
-      s2len = num;
-    }
-  for (num = 0; s1[num] != '\0'; num++)
-    {
-      s1len = num;
-    }
-  if (n >= s2len)
-    {
-      array = malloc(s2len + s1len);
-    }
-  else
-    {
-      array = malloc(s1len + n);
-    }
-  if (array == NULL)
-    {
-      free(array);
-      return (NULL);
-    }
-  for (num = 0; s1[num] != '\0'; num++)
-    {
-      array[num] = s1[num];
-    }
-  if (n >= s2len)
-    {
-      for (num = 0; s2[num]; num++)
-	{
-	  array[num + s1len] = s2[num];
-	}
-    }
-  else
-    {
-      for (num = 0; num < n; num++)
-	{
-	  array[num + s1len] = s2[num];
-	}
-    }
-  return (array);
+char *array;
+unsigned int num, s1len, s2len;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+s2len = _strlen(s2);
+s1len = _strlen(s1);
+if (n >= s2len)
+{
+array = malloc(s2len + s1len);
+}
+else
+{
+array = malloc(s1len + n);
+}
+if (array == NULL)
+{
+free(array);
+return (NULL);
+}
+for (num = 0; s1[num] != '\0'; num++)
+{
+array[num] = s1[num];
+}
+if (n >= s2len)
+{
+for (num = 0; s2[num]; num++)
+{
+array[num + s1len] = s2[num];
+}
+}
+else
+{
+for (num = 0; num < n; num++)
+{
+array[num + s1len] = s2[num];
+}
+}
+return (array);
+}
+
+/**
+ * _strlen - gets length of string
+ * @c: string
+ * Return: unsigned return
+ */
+unsigned int _strlen(char *c)
+{
+unsigned int num, temp;
+for (num = 0; c[num] != '\0'; num++)
+{
+temp = num;
+}
+return (temp);
 }
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+unsigned int _strlen(char *c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 #endif
