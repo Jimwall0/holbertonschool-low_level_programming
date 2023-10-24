95910d8474bd0dd707304bcc04c69e95d7c9ae07
Tsim <tyang4204@gmail.com>
2023-10-23 17:56:39 -0700

    betty

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,19 +1,22 @@
 #include "main.h"
 /**
- * _calloc - ...
+ * _calloc - initialize and assignes 0
+ * @nmemb: number of elements
+ * @size: the amount of mememory for each element
+ * Return: pointer
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  char *array;
-  if (nmemb == 0 || size == 0)
-    {
-      return (NULL);
-    }
-  array = calloc(nmemb, size);
-  if (array == NULL)
-    {
-      free(array);
-      return (NULL);
-    }
-  return (array);
+char *array;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+array = calloc(nmemb, size);
+if (array == NULL)
+{
+free(array);
+return (NULL);
+}
+return (array);
 }
