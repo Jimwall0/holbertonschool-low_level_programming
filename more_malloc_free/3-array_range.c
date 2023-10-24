fc6f58ca6f284339b1d25d0f03eb3dd59547012a
Tsim <tyang4204@gmail.com>
2023-10-23 19:08:02 -0700

    finished

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -4,21 +4,21 @@
  */
 int *array_range(int min, int max)
 {
-  int *array, num = 0;
-  if (min > max)
-    {
-      return (NULL);
-    }
-  array = malloc(sizeof(int) * (max - min + 1));
-  if (array == NULL)
-    {
-      free(array);
-      return (NULL);
-    }
-  for (; min <= max; min++)
-    {
-      array[num] = min;
-      num++;
-    }
-  return (array);
+int *array, num = 0;
+if (min > max)
+{
+return (NULL);
+}
+array = malloc(sizeof(int) * (max - min + 1));
+if (array == NULL)
+{
+free(array);
+return (NULL);
+}
+for (; min <= max; min++)
+{
+array[num] = min;
+num++;
+}
+return (array);
 }
