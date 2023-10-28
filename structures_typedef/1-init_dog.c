2a487cc0c22f7981b5931cd7985374d18ae73a06
Tsim <tyang4204@gmail.com>
2023-10-28 09:59:09 -0700

    finished 1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -8,8 +8,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-struct dog dog;
-dog.name->name;
-dog.age->age;
-dog.owner->owner;
+struct dog;
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
