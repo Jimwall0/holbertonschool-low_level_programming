f1513cbafaba0c92e2f9f2ad4cb30bd1261e9c1d
Tsim <tyang4204@gmail.com>
2023-10-28 09:25:15 -0700

    betty updated

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - struct woth name, age, owner
+ * @name: a name for dog
+ * @age: how old is the dog
+ * @owner: who owns the dog
+ */
+
 struct dog
 {
 char *name;
