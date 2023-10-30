3cce2bc8f8e4094aaedd14955647acb4e6c167b9
Tsim <tyang4204@gmail.com>
2023-10-30 12:46:21 -0700

    added variables to store arrays

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,13 +9,15 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
   dog_t *dog1;
+  char *temp1 = name, *temp2 = owner;
   dog1 = malloc(sizeof(dog_t));
   if (dog1 == NULL)
     {
+      free(dog1);
       return (NULL);
     }
-  dog1->name = name;
+  dog1->name = temp1;
   dog1->age = age;
-  dog1->owner = owner;
+  dog1->owner = temp2;
   return (dog1);
 }
