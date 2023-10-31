5ec8f7f6ac43a4e2582c20d28963f2a4a5bf4165
Tsim <tyang4204@gmail.com>
2023-10-30 18:19:05 -0700

    added memory for two char string

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,8 +16,30 @@ dog_t *new_dog(char *name, float age, char *owner)
       free(dog1);
       return (NULL);
     }
+  temp1 = malloc(sizeof(char) * _strlen(name));
+  if (temp1 == NULL)
+    {
+      free(temp1);
+      return (NULL);
+    }
+  temp2 = malloc(sizeof(char) * _strlen(owner));
+  if (temp2 == NULL)
+    {
+      free(temp1);
+      return (NULL);
+    }
   dog1->name = temp1;
   dog1->age = age;
   dog1->owner = temp2;
   return (dog1);
+}
+
+int _strlen(char *s)
+{
+  int temp = 0;
+  while (s[temp] != '\0')
+    {
+      temp++;
+    }
+  return (temp);
 }
