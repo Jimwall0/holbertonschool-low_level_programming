51be9f4b1e39bc6eb75a722767093e180c0cec94
Tsim <tyang4204@gmail.com>
2023-11-01 11:47:10 -0700

    done

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,12 +1,13 @@
 #include "function_pointers.h"
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-  if (array != NULL || action != NULL)
-    {
   long unsigned int num;
+  if (array == NULL || action == NULL)
+    {
+      return;
+    }
   for (num = 0; num < size; num++)
     {
       action(array[num]);
-    }
     }
 }
