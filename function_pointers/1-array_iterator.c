9a9b94286e3eb4cec255792000efaf63c8ed0ef4
Tsim <tyang4204@gmail.com>
2023-11-01 11:28:29 -0700

    forgot about NULL check

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,9 +1,12 @@
 #include "function_pointers.h"
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+  if (array != NULL)
+    {
   long unsigned int num;
   for (num = 0; num < size; num++)
     {
       action(array[num]);
+    }
     }
 }
