4f97bf0582086729e643e89da451da6ba9c1f039
Tsim <tyang4204@gmail.com>
2023-11-03 08:02:05 -0700

    success

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+  unsigned int num, result;
+  va_list numbers;
+  va_start(numbers, n);
+  if (separator == NULL)
+    {
+      for (num = 0; num < n; num++)
+	{
+	  result += va_arg(numbers, const unsigned int);
+	  printf("%d", result);
+	}
+    }
+  else
+    {
+      for (num = 0; num < n; num++)
+        {
+          result = va_arg(numbers, const unsigned int);
+          printf("%d", result);
+	  if (num != n - 1)
+	    {
+	      printf("%s", separator);
+	    }
+        }
+    }
+  printf("\n");
+  va_end(numbers);
+}
