7ce85711fc655d6048edb9d6995cd5dacdf6b46e
Tsim <tyang4204@gmail.com>
2023-11-27 12:03:58 -0800

    added library

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 /**
  * add_node - adds a node
  * @head: new head?
@@ -7,7 +7,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
   /*initialize node*/
-  list_t unsigned node;
+  unsigned lists_t node;
   node = malloc(sizeof(struct list_t));
   if (node == NULL)
     {
