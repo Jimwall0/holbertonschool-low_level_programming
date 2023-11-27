d63fcbd315e2fc7867679ddde14031dc967c13ff
Tsim <tyang4204@gmail.com>
2023-11-27 11:19:24 -0800

    added more checks

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,10 +9,18 @@ size_t print_list(const list_t *h)
 /*temp for number of nodes*/
 unsigned long int num = 0;
 const list_t *temp = h;
-/*loop through node and prints*/
+/*checks and loop temp*/
 while (temp != NULL)
 {
+/*checks string*/
+if (temp->str != NULL)
+{
 printf("[%i] %s\n", temp->len, temp->str);
+}
+else
+{
+printf("[%i] (nil)\n", temp->len);
+}
 num++;
 temp = temp->next;
 }
